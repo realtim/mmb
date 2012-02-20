085061452c628e40b5a2909a526cee88f3f7dda0
Leonid Fishkis <leonidfishkis@gmail.com>
2012-02-21 00:38:56 +0400

    #23

--- site/menu.php
+++ site/menu.php
@@ -309,6 +309,7 @@
         print('<tr><td><a href="https://github.com/realtim/mmb/wiki/%D0%A1%D0%B2%D0%B5%D0%B4%D0%B5%D0%BD%D0%B8%D1%8F-%D0%BE-%D1%81%D0%B5%D1%80%D0%B2%D0%B8%D1%81%D0%B5-%D0%9C%D0%9C%D0%91"  target = "_blank">О сервисе</a>, 
                        <a href="https://github.com/realtim/mmb/wiki/%D0%90%D0%B2%D1%82%D0%BE%D1%80%D1%8B" target = "_blank">Авторы</a>
                </td></tr>'."\r\n"); 
+	print('<tr><td><a href="https://github.com/realtim/mmb/wiki/%D0%92%D0%BE%D0%BF%D1%80%D0%BE%D1%81%D1%8B-%D0%B8-%D0%BE%D1%82%D0%B2%D0%B5%D1%82%D1%8B"  target = "_blank">Вопросы и ответы</a></td></tr>'."\r\n"); 
 	print('</table>'."\r\n");
 
 	
