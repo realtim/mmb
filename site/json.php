563a700d99b6eb31406590ae3f9e62a1450aca0d
Tim Alexeevskiy <realtim@gmail.com>
2012-05-02 11:28:55 +0400

    Добавил генерацию всех необходимых данных #46

--- site/json.php
+++ site/json.php
@@ -34,17 +34,54 @@ if ($Row['user_admin'] == 0)
 }
 
 // Сбор данных для дампа
-
 $data = array();
 
+// Raids: raid_id, raid_name, raid_registrationenddate
 $Sql = "select raid_id, raid_name, raid_registrationenddate from Raids";
-$Result = MySqlQuery($Sql);  
+$Result = MySqlQuery($Sql);
 while ( ( $Row = mysql_fetch_assoc($Result) ) ) { $data["Raids"][] = $Row; }
 
+// Distances: distance_id, raid_id, distance_name
 $Sql = "select distance_id, raid_id, distance_name from Distances";
-$Result = MySqlQuery($Sql);  
+$Result = MySqlQuery($Sql);
 while ( ( $Row = mysql_fetch_assoc($Result) ) ) { $data["Distances"][] = $Row; }
 
+// Levels: level_id, distance_id, level_name, level_order, level_starttype, level_pointnames, level_pointpenalties, level_begtime, level_maxbegtime, level_minendtime, level_endtime
+$Sql = "select level_id, distance_id, level_name, level_order, level_starttype, level_pointnames, level_pointpenalties, level_begtime, level_maxbegtime, level_minendtime, level_endtime from Levels";
+$Result = MySqlQuery($Sql);
+while ( ( $Row = mysql_fetch_assoc($Result) ) ) { $data["Levels"][] = $Row; }
+
+// LevelPoints: levelpoint_id, level_id, pointtype_id
+$Sql = "select levelpoint_id, level_id, pointtype_id from LevelPoints";
+$Result = MySqlQuery($Sql);
+while ( ( $Row = mysql_fetch_assoc($Result) ) ) { $data["LevelPoints"][] = $Row; }
+
+// Teams: team_id, distance_id, team_name, team_num
+$Sql = "select team_id, distance_id, team_name, team_num from Teams";
+$Result = MySqlQuery($Sql);
+while ( ( $Row = mysql_fetch_assoc($Result) ) ) { $data["Teams"][] = $Row; }
+
+// Users: user_id, user_name, user_birthyear
+$Sql = "select user_id, user_name, user_birthyear from Users";
+$Result = MySqlQuery($Sql);
+while ( ( $Row = mysql_fetch_assoc($Result) ) ) { $data["Users"][] = $Row; }
+
+// TeamUsers: teamuser_id, team_id, user_id, teamuser_hide
+$Sql = "select teamuser_id, team_id, user_id, teamuser_hide from TeamUsers";
+$Result = MySqlQuery($Sql);
+while ( ( $Row = mysql_fetch_assoc($Result) ) ) { $data["TeamUsers"][] = $Row; }
+
+// TeamLevelDismiss: user_id, levelpoint_id, team_id, teamuser_id, teamleveldismiss_date, device_id
+$Sql = "select user_id, levelpoint_id, team_id, teamuser_id, teamleveldismiss_date, device_id from TeamLevelDismiss";
+$Result = MySqlQuery($Sql);
+while ( ( $Row = mysql_fetch_assoc($Result) ) ) { $data["TeamLevelDismiss"][] = $Row; }
+
+// TeamLevelPoints: user_id, levelpoint_id, team_id, teamlevelpoint_date, device_id, teamlevelpoint_datetime, teamlevelpoint_points, teamlevelpoint_comment
+$Sql = "select user_id, levelpoint_id, team_id, teamlevelpoint_date, device_id, teamlevelpoint_datetime, teamlevelpoint_points, teamlevelpoint_comment from TeamLevelPoints";
+$Result = MySqlQuery($Sql);
+while ( ( $Row = mysql_fetch_assoc($Result) ) ) { $data["TeamLevelPoints"][] = $Row; }
+
+// Вывод json
 print json_encode( $data );
 
 ?>
