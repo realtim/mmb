0ae31f7ada848262169005a86879cbac9d6682b5
Leonid Fishkis <leonidfishkis@gmail.com>
2015-11-06 09:23:44 +0100

    Update viewuserdata.php

--- site/viewuserdata.php
+++ site/viewuserdata.php
@@ -583,7 +583,7 @@ if (!isset($MyPHPScript)) return;
 		while ($Row = mysql_fetch_assoc($Result))
 		{
 			$linktypeselected = '';
-			$LinkNameDisabled =  (empty($Row['linktype_textonly']) ? 'off', 'on');
+			$LinkNameDisabled =  (empty($Row['linktype_textonly']) ? 'off' : 'on');
 			print('<option value="'.$Row['linktype_id'].'" '.$linktypeselected.'  onclick = "javascript:document..UserLinksForm.NewLinkName.disabled='.$LinkNameDisabled.';">'.$Row['linktype_name']."</option>\n");
 		}
 		mysql_free_result($Result);
