02f25a8f58ea2d10720f6ceccb2b3f7be76e3c84
Leonid Fishkis <leonidfishkis@gmail.com>
2016-03-21 14:31:57 +0100

    Update functions.php

--- site/functions.php
+++ site/functions.php
@@ -244,7 +244,7 @@ class CSql {
 				inner join Distances d on t.distance_id = d.distance_id
 			where tu.teamuser_hide = 0 and t.team_hide = 0 and d.raid_id = $raidId and tu.user_id = $userId";
 
-		echo $sql; 
+	//	echo $sql; 
 		return self::singleValue($sql, 'team_id');
 	}
 
