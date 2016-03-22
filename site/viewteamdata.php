abba4053c3f3a006da8d99276a83b6e660283325
Leonid Fishkis <leonidfishkis@gmail.com>
2016-03-22 23:18:05 +0100

    #158

--- site/viewteamdata.php
+++ site/viewteamdata.php
@@ -357,7 +357,7 @@ print('Дистанция '."\n");
 if (!$DisabledDistance) {
 
 	// Показываем выпадающий список дистанций
-	print('<select name="DistanceId" class="leftmargin" tabindex="'.(++$TabIndex).'" '.$DisabledDistanceText.'>'."\n");
+	print('<select name="DistanceId" class="leftmargin" tabindex="'.(++$TabIndex).'">'."\n");
 	$sql = "select distance_id, distance_name from Distances where distance_hide = 0 and raid_id = $RaidId";
 	$Result = MySqlQuery($sql);
 	while ($Row = mysql_fetch_assoc($Result))
