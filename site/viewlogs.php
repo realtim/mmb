ce6cf6e511dfd0ca38c1900b6599ef60642865a8
Serge Titov <se-ti>
2016-04-12 10:04:50 +0300

    logs search

--- site/viewlogs.php
+++ site/viewlogs.php
@@ -12,7 +12,6 @@ if (!isset($MyPHPScript)) return;
 
 <form name="LogsForm" action="<? echo $MyPHPScript; ?>" method="post">
     <input type="hidden" value="viewLogs" name="action"/>
-
     <div style="margin-bottom: 2ex;">
 
 <?php
@@ -21,18 +20,14 @@ if (!isset($MyPHPScript)) return;
     $allLevels = array(CMmbLogger::Trace, CMmbLogger::Debug, CMmbLogger::Info, CMmbLogger::Error, CMmbLogger::Critical);
     $levels = array();
     $rawLevels = mmb_validate($_REQUEST, 'levels', array());
-    if (!is_array($rawLevels ))
-    {
-        print($rawLevels . "<br/>");
+    if (!is_array($rawLevels))
         $rawLevels = array($rawLevels);
-    }
-
 
     foreach ($allLevels as $lev)
         if (in_array($lev, $rawLevels))
             $levels[] = $lev;
 
-    print("Типы сообщений: <select name=\"levels\" size=\"5\" multiple style=\"margin-left: 10px; margin-right: 5px; vertical-align: top;\"
+    print("Типы сообщений: <select name=\"levels[]\" size=\"5\" multiple style=\"margin-left: 10px; margin-right: 5px; vertical-align: top;\"
             onchange=\"document.LogsForm.submit();\">");
     foreach ($allLevels as $lev)
     {
@@ -51,13 +46,18 @@ if (!isset($MyPHPScript)) return;
     }
     print("</select>\n");
 
+
+    $searchVal = mmb_validate($_REQUEST, 'search', '');
+    print("<input type=\"text\" placeholder=\"Искать\" name=\"search\" onchange=\"document.LogsForm.submit()\" style=\"margin-left: 2em;\" value='". CMmbUI::toHtml($searchVal). "'/>");
+
     print("</div>\n");
 
 
     $cond = count($levels) == 0 ? 'true' : "logs_level in ('" . implode("', '", $levels) . "')";
+    $searchCond = $searchVal == '' ? 'true' : "logs_message like(".CSql::quote("%$searchVal%").")";
 
     $sql = "select logs_id, logs_level, user_id, logs_operation, logs_message, logs_dt from Logs 
-        where $cond 
+        where $cond and $searchCond 
         order by logs_id desc 
         limit $limit";
 
@@ -66,7 +66,7 @@ if (!isset($MyPHPScript)) return;
     print("<table class='std'>\n");
     print("<tr class='gray head'><th width='50'>id</th><th>Время</th><th>Уровень</th><th>Пользователь</th><th>Операция</th><th>Сообщение</th><th>Длительность</th></tr>\n");
     while ($Row = mysql_fetch_assoc($Result))
-        print("<tr><td>{$Row['logs_id']}</td><td>" . $Row['logs_dt'] /*date("Y-m-d hh:mm:ss", $Row['logs_dt'])*/ . "</td><td>{$Row['logs_level']}</td><td>{$Row['user_id']}</td><td>{$Row['logs_operation']}</td><td>{$Row['logs_message']}</td><td> </td></tr>\n"); // <td>{$Row['logs_duration']}</td>
+        print("<tr><td>{$Row['logs_id']}</td><td>" . $Row['logs_dt'] /*date("Y-m-d hh:mm:ss", $Row['logs_dt'])*/ . "</td><td>{$Row['logs_level']}</td><td>{$Row['user_id']}</td><td>{$Row['logs_operation']}</td><td>". nl2br($Row['logs_message']) ."</td><td> </td></tr>\n"); // <td>{$Row['logs_duration']}</td>
 
     mysql_free_result($Result);
     print("</table>");
