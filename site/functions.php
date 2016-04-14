a921de6ebc3a74c3da074f7707c5561c956dcf85
Serge Titov <se-ti>
2016-04-14 10:34:18 +0300

    duplicate func fix

--- site/functions.php
+++ site/functions.php
@@ -3450,17 +3450,6 @@ class CMmbLogger
 		}
 	}
 
-	private static function initVars()
-	{
-		if (self::$minLevelCode !== null && self::$fatalErrorMail !== null)
-			return;
-
-		include("settings.php");
-
-		self::$minLevelCode = self::levelCode($MinLogLevel);
-		self::$fatalErrorMail = $FatalErrorMail;
-	}
-
 	private static function levelCode($level)
 	{
 		switch($level)
