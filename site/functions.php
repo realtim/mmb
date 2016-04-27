70230d09b5592c0378d9bc0123559e0b47951923
Leonid Fishkis <leonidfishkis@gmail.com>
2016-04-27 21:36:00 +0200

    #114
    добавил функцию  image_resize

--- site/functions.php
+++ site/functions.php
@@ -3477,4 +3477,83 @@ class CMmbLogger
 	}
 }
 
+
+// Изменение размера
+function image_resize($src, $dst, $width, $height, $crop=0){
+
+  if(!list($w, $h) = getimagesize($src)) return "Unsupported picture type!";
+
+  $type = strtolower(substr(strrchr($src,"."),1));
+  if($type == 'jpeg') $type = 'jpg';
+  switch($type){
+    case 'bmp': $img = imagecreatefromwbmp($src); break;
+    case 'gif': $img = imagecreatefromgif($src); break;
+    case 'jpg': $img = imagecreatefromjpeg($src); break;
+    case 'png': $img = imagecreatefrompng($src); break;
+    default : return "Unsupported picture type!";
+  }
+
+  // resize
+  if($crop){
+    if($w < $width or $h < $height) return "Picture is too small!";
+    $ratio = max($width/$w, $height/$h);
+    $h = $height / $ratio;
+    $x = ($w - $width / $ratio) / 2;
+    $w = $width / $ratio;
+  }
+  else{
+    if($w < $width and $h < $height) return "Picture is too small!";
+    $ratio = min($width/$w, $height/$h);
+    $width = $w * $ratio;
+    $height = $h * $ratio;
+    $x = 0;
+  }
+
+  $new = imagecreatetruecolor($width, $height);
+
+  // preserve transparency
+  if($type == "gif" or $type == "png"){
+    imagecolortransparent($new, imagecolorallocatealpha($new, 0, 0, 0, 127));
+    imagealphablending($new, false);
+    imagesavealpha($new, true);
+  }
+
+  imagecopyresampled($new, $img, 0, 0, $x, 0, $width, $height, $w, $h);
+  imagedestroy($img);
+
+  if  (!empty($dst))
+  {
+	switch($type)
+	{
+		case 'bmp': imagewbmp($new, $dst); break;
+		case 'gif': imagegif($new, $dst); break;
+		case 'jpg': imagejpeg($new, $dst); break;
+    		case 'png': imagepng($new, $dst); break;
+  	}
+
+  } else {
+  	
+	switch($type)
+	{
+		case 'bmp': header('Content-Type: image/bmp'); break;
+		case 'gif': header('Content-Type: image/gif'); break;
+		case 'jpg': header('Content-Type: image/jpeg'); break;
+    		case 'png': header('Content-Type: image/png'); break;
+  	}
+
+	switch($type)
+	{
+		case 'bmp': imagewbmp($new); break;
+		case 'gif': imagegif($new); break;
+		case 'jpg': imagejpeg($new); break;
+    		case 'png': imagepng($new); break;
+  	}
+
+  }
+  // конец разбора, как выводить изображение 
+  imagedestroy($new);
+  return true;
+}
+
+
 ?>
