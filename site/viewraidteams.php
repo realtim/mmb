6515c7c1ce994e22f06dbb1c59fafb287db06929
Leonid Fishkis <leonidfishkis@gmail.com>
2016-04-27 22:13:18 +0200

    #114

--- site/viewraidteams.php
+++ site/viewraidteams.php
@@ -520,7 +520,7 @@
 			//	echo $point.' '.$ImageLink.' '.$tumbImg;
 		          if (!is_file($tumbImg))
 		          {
-			     image_resize(trim($MyStoreHttpLink).trim($ImageLink), trim($MyStoreHttpLink).trim($tumbImg), 1000, 100, 0);
+			     image_resize(trim($MyStoreFileLink).trim($ImageLink), trim($MyStoreFileLink).trim($tumbImg), 1000, 100, 0);
 		          }
 			  print('<a style="margin-right: 15px;" href="'.trim($MyStoreHttpLink).trim($ImageLink).'" title="'.trim($ImageComment).'" target = "_blank"><img src = "'.trim($MyStoreHttpLink).trim($tumbImg).'"  alt = "'.trim($ImageComment).'" height = "100"></a>'."\r\n");
 		        }
