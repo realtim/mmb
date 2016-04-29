812c3cb0a570cc70fcf1ee7cb0bf8c5ed74cf3f6
Serge Titov <se-ti>
2016-04-29 08:58:18 +0300

    refactor image resize и отступы

--- site/functions.php
+++ site/functions.php
@@ -3479,81 +3479,63 @@ class CMmbLogger
 
 
 // Изменение размера
-function image_resize($src, $dst, $width, $height, $crop=0){
+function image_resize($src, $dst, $width, $height, $crop=0)
+{
+	if(!list($w, $h) = getimagesize($src)) return "Unsupported picture type!";
 
-  if(!list($w, $h) = getimagesize($src)) return "Unsupported picture type!";
-
-  $type = strtolower(substr(strrchr($src,"."),1));
-  if($type == 'jpeg') $type = 'jpg';
-  switch($type){
-    case 'bmp': $img = imagecreatefromwbmp($src); break;
-    case 'gif': $img = imagecreatefromgif($src); break;
-    case 'jpg': $img = imagecreatefromjpeg($src); break;
-    case 'png': $img = imagecreatefrompng($src); break;
-    default : return "Unsupported picture type!";
-  }
-
-  // resize
-  if($crop){
-    if($w < $width or $h < $height) return "Picture is too small!";
-    $ratio = max($width/$w, $height/$h);
-    $h = $height / $ratio;
-    $x = ($w - $width / $ratio) / 2;
-    $w = $width / $ratio;
-  }
-  else{
-    if($w < $width and $h < $height) return "Picture is too small!";
-    $ratio = min($width/$w, $height/$h);
-    $width = $w * $ratio;
-    $height = $h * $ratio;
-    $x = 0;
-  }
-
-  $new = imagecreatetruecolor($width, $height);
-
-  // preserve transparency
-  if($type == "gif" or $type == "png"){
-    imagecolortransparent($new, imagecolorallocatealpha($new, 0, 0, 0, 127));
-    imagealphablending($new, false);
-    imagesavealpha($new, true);
-  }
-
-  imagecopyresampled($new, $img, 0, 0, $x, 0, $width, $height, $w, $h);
-  imagedestroy($img);
-
-  if  (!empty($dst))
-  {
+	$type = strtolower(substr(strrchr($src,"."),1));
+	// инициализируем интерфейс
 	switch($type)
 	{
-		case 'bmp': imagewbmp($new, $dst); break;
-		case 'gif': imagegif($new, $dst); break;
-		case 'jpg': imagejpeg($new, $dst); break;
-    		case 'png': imagepng($new, $dst); break;
-  	}
+		case 'bmp': $image = array('mime' => 'image/bmp',  'create' => 'imagecreatefromwbmp', 'write' => 'imagewbmp'); break;
+		case 'gif': $image = array('mime' => 'image/gif',  'create' => 'imagecreatefromgif',  'write' => 'imagegif');  break;
+		case 'jpeg':
+		case 'jpg': $image = array('mime' => 'image/jpeg', 'create' => 'imagecreatefromjpeg', 'write' => 'imagejpeg'); break;
+		case 'png': $image = array('mime' => 'image/png',  'create' => 'imagecreatefrompng',  'write' => 'imagepng');  break;
+		default : return "Unsupported picture type!";
+	}
 
-  } else {
-  	
-	switch($type)
+	// resize
+	if($crop)
 	{
-		case 'bmp': header('Content-Type: image/bmp'); break;
-		case 'gif': header('Content-Type: image/gif'); break;
-		case 'jpg': header('Content-Type: image/jpeg'); break;
-    		case 'png': header('Content-Type: image/png'); break;
-  	}
-
-	switch($type)
+		if($w < $width or $h < $height) return "Picture is too small!";
+		$ratio = max($width/$w, $height/$h);
+		$h = $height / $ratio;
+		$x = ($w - $width / $ratio) / 2;
+		$w = $width / $ratio;
+	}
+	else
 	{
-		case 'bmp': imagewbmp($new); break;
-		case 'gif': imagegif($new); break;
-		case 'jpg': imagejpeg($new); break;
-    		case 'png': imagepng($new); break;
-  	}
+		if($w < $width and $h < $height) return "Picture is too small!";
+		$ratio = min($width/$w, $height/$h);
+		$width = $w * $ratio;
+		$height = $h * $ratio;
+		$x = 0;
+	}
 
-  }
-  // конец разбора, как выводить изображение 
-  imagedestroy($new);
-  return true;
+	$new = imagecreatetruecolor($width, $height);
+
+	// preserve transparency
+	if($type == "gif" or $type == "png")
+	{
+		imagecolortransparent($new, imagecolorallocatealpha($new, 0, 0, 0, 127));
+		imagealphablending($new, false);
+		imagesavealpha($new, true);
+	}
+
+
+	if  (!empty($dst))
+		$image['write']($new, $dst);
+	else
+	{
+		header('Content-Type: ' . $image['mime']);
+		$image['write']($new);
+	}
+
+
+	// конец разбора, как выводить изображение
+	imagedestroy($new);
+	return true;
 }
-
 
 ?>
