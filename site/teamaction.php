da1241cd46d9f1ade4ad2ba9689d5fad25b80ad4
Leonid Fishkis <leonidfishkis@gmail.com>
2016-06-09 20:18:06 +0200

    #202

--- site/teamaction.php
+++ site/teamaction.php
@@ -233,7 +233,7 @@ elseif ($action == 'TeamChangeData' or $action == "AddTeam")
 	$TeamUserNew = 0;
 	if  ($NewUserId > 0) {
 		$NotStartPreviousRaidId = CheckNotStart($NewUserId, $RaidId);	
-		$TeamUserNew = (CSql::userRaidsCount($NewUserId) > 0) ? 1 : 0;
+		$TeamUserNew = (CSql::userRaidsCount($NewUserId) > 0) ? 0 : 1;
 	}
 	
 	
