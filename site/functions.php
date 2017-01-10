42bacb5cd0aea08e480331599336ec63551681ef
Leonid Fishkis <leonidfishkis@gmail.com>
2017-01-10 12:27:19 +0100

    #207

--- site/functions.php
+++ site/functions.php
@@ -784,7 +784,7 @@ class CMmbAuth {
 		    $Subject,
 		    $Message."\r\n".'Используйте для вопросов адрес mmbsite@googlegroups.com'."\r\n".'Ответ на это письмо будет проигнорирован.'."\r\n",
 		    FALSE,
-		    'no-reply@progressor.ru');
+		    'noreply@progressor.ru');
     }
 
 
