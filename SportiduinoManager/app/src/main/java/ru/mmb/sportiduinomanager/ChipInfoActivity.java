896f5c901ce48f7fb1a3a0dd874ccba13f2b5041
yuriyweiss <gweiss@yandex.ru>
2019-04-25 12:21:08 +0300

    [#298] ChipInfoActivity team mask decoded with member names and activity sign

--- SportiduinoManager/app/src/main/java/ru/mmb/sportiduinomanager/ChipInfoActivity.java
+++ SportiduinoManager/app/src/main/java/ru/mmb/sportiduinomanager/ChipInfoActivity.java
@@ -10,9 +10,11 @@ import android.widget.Toast;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 import ru.mmb.sportiduinomanager.model.Station;
+import ru.mmb.sportiduinomanager.model.Teams;
 import ru.mmb.sportiduinomanager.task.ChipInfoTask;
 
 import static ru.mmb.sportiduinomanager.model.Station.UID_SIZE;
@@ -125,32 +127,21 @@ public final class ChipInfoActivity extends MainActivity {
         if (chipInfo == null) return "";
         final int pagesCount = (chipInfo.length - UID_SIZE) / 5;
         final StringBuilder builder = new StringBuilder(1024);
+        int teamNum = -1;
         for (int i = 0; i < pagesCount; i++) {
             final int pos = UID_SIZE + i * 5;
             builder.append(String.format("Page#%2d: %02X %02X %02X %02X", chipInfo[pos],
                     chipInfo[pos + 1], chipInfo[pos + 2], chipInfo[pos + 3], chipInfo[pos + 4]));
             if (i == 4) {
-                final int teamNum = Station.byteArray2Int(chipInfo, pos + 1, pos + 2);
-                final int ntag = chipInfo[pos + 3] & 0xFF;
-                final int version = chipInfo[pos + 4] & 0xFF;
-                builder.append(String.format("\tTeam# %d, Ntag %d, version %d",
-                        teamNum, ntag, version));
+                teamNum = buildPage4DecodedInfo(chipInfo, builder, pos);
             } else if (i == 5) {
-                final int initTime = Station.byteArray2Int(chipInfo, pos + 1, pos + 4);
-                builder.append("\tInitTime: ")
-                        .append(DATE_FORMAT.get().format(new Date(initTime * 1000L)));
+                buildPage5DecodedInfo(chipInfo, builder, pos);
             } else if (i == 6) {
-                final int teamMask = Station.byteArray2Int(chipInfo, pos + 1, pos + 2);
-                builder.append("\tTeamMask: ").append(Integer.toBinaryString(teamMask));
+                buildPage6DecodedInfo(chipInfo, builder, teamNum, pos);
             } else if (i >= 8 && !(chipInfo[pos + 1] == 0 && chipInfo[pos + 2] == 0
                     && chipInfo[pos + 3] == 0 && chipInfo[pos + 4] == 0)) {
                 // If all bytes are 0, then don't try to format data
-                final int pointNumber = chipInfo[pos + 1] & 0xFF;
-                final int todayUnix = (int) (System.currentTimeMillis() / 1000L);
-                final int todayUpperByte = todayUnix & 0xFF000000;
-                final int pointTime = Station.byteArray2Int(chipInfo, pos + 2, pos + 4) + todayUpperByte;
-                builder.append("\tKP# ").append(pointNumber).append(", PointTime: ")
-                        .append(DATE_FORMAT.get().format(new Date(pointTime * 1000L)));
+                buildKpPageDecodedInfo(chipInfo, builder, pos);
             }
             builder.append('\n');
         }
@@ -158,7 +149,85 @@ public final class ChipInfoActivity extends MainActivity {
     }
 
     /**
-     * Refresh activity controls state.
+     * Build info for teamNumber, Ntag and software version.
+     *
+     * @param chipInfo read_card_page response
+     * @param builder  target StringBuilder to append text
+     * @param pos      current page start position
+     * @return decoded team number
+     */
+    private int buildPage4DecodedInfo(final byte[] chipInfo, final StringBuilder builder, final int pos) {
+        final int teamNum = Station.byteArray2Int(chipInfo, pos + 1, pos + 2);
+        final int ntag = chipInfo[pos + 3] & 0xFF;
+        final int version = chipInfo[pos + 4] & 0xFF;
+        builder.append(String.format("\n\tTeam# %d, Ntag %d, version %d", teamNum, ntag, version));
+        return teamNum;
+    }
+
+    /**
+     * Build info for chip init time.
+     *
+     * @param chipInfo read_card_page response
+     * @param builder  target StringBuilder to append text
+     * @param pos      current page start position
+     */
+    private void buildPage5DecodedInfo(final byte[] chipInfo, final StringBuilder builder, final int pos) {
+        final int initTime = Station.byteArray2Int(chipInfo, pos + 1, pos + 4);
+        builder.append("\n\tInitTime: ").append(DATE_FORMAT.get().format(new Date(initTime * 1000L)));
+    }
+
+    /**
+     * Build teamMask decoded info.
+     *
+     * @param chipInfo read_card_page response
+     * @param builder  target StringBuilder to append text
+     * @param teamNum  current team number
+     * @param pos      current page start position
+     */
+    private void buildPage6DecodedInfo(final byte[] chipInfo, final StringBuilder builder,
+                                       final int teamNum, final int pos) {
+        final int teamMask = Station.byteArray2Int(chipInfo, pos + 1, pos + 2);
+        builder.append("\n\tTeamMask [").append(Integer.toBinaryString(teamMask))
+                .append("] to members:");
+        buildTeamMembersInfo(builder, teamNum, teamMask);
+    }
+
+    /**
+     * Append to builder pretty text with current members status in team.
+     *
+     * @param builder  target StringBuilder to append text
+     * @param teamNum  team number, -1 - no team selected
+     * @param teamMask active members mask
+     */
+    private void buildTeamMembersInfo(final StringBuilder builder, final int teamNum, final int teamMask) {
+        final Teams teams = mMainApplication.getTeams();
+        final List<String> teamMembers = teams.getMembersNames(teamNum);
+        if (teamMembers.isEmpty()) return;
+        for (int i = 0; i < teamMembers.size(); i++) {
+            final String teamMember = teamMembers.get(i);
+            final String memberActivity = ((teamMask & (1 << i)) == 0) ? "(-)" : "(+)";
+            builder.append("\n\t\t").append(teamMember).append(' ').append(memberActivity);
+        }
+    }
+
+    /**
+     * Build active point decoded info.
+     *
+     * @param chipInfo read_card_page response
+     * @param builder  target StringBuilder to append text
+     * @param pos      current page start position
+     */
+    private void buildKpPageDecodedInfo(final byte[] chipInfo, final StringBuilder builder, final int pos) {
+        final int pointNumber = chipInfo[pos + 1] & 0xFF;
+        final int todayUnix = (int) (System.currentTimeMillis() / 1000L);
+        final int todayUpperByte = todayUnix & 0xFF000000;
+        final int pointTime = Station.byteArray2Int(chipInfo, pos + 2, pos + 4) + todayUpperByte;
+        builder.append("\n\tKP# ").append(pointNumber).append(", PointTime: ")
+                .append(DATE_FORMAT.get().format(new Date(pointTime * 1000L)));
+    }
+
+    /**
+     * Refresh UI controls state.
      */
     public void updateLayout() {
         final Button infoButton = findViewById(R.id.chip_info_request);
