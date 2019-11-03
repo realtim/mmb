8236d8fd7f96c5f3d7236a8428a0fa062f04bf66
Leonid Fishkis <leonidfishkis@gmail.com>
2019-11-03 20:35:49 +0100

    test3

--- site/sql/call_usp_editUser.sql
+++ site/sql/call_usp_editUser.sql
@@ -3,3 +3,4 @@ set  @sessionUid = 'aeb99e9d-f43a-11e9-9666-ed80b7164828';
 CALL usp_editUser(@executionUid, @sessionUid, 30, 'ttt@ttt.com', 'Казанцев Владимир', 1986, 'Железнодорожный');
 select @executionUid, @sessionUid;
 call usp_viewLogs(@executionUid, null);
+set  @executionUid = null;
